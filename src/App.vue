3c4f96a747a5e91cfc04bc59875a46ecbcfaa7c6
MT-Lin <mingtielin@outlook.com>
2017-12-31 19:12:43 +0800

    修改UI默认大小

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+/*
+ * @Author: mingtielin@outlook.com 
+ * @Date: 2017-12-30 23:08:33 
+ * @Last Modified by:   mingtielin@outlook.com 
+ * @Last Modified time: 2017-12-30 23:08:33 
+ */
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
